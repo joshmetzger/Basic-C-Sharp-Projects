03299109a4c7edd4d580f71ddcac0028c5e62be4
joshmetzger <joshmetzger@gmail.com>
2025-06-13 08:54:04 -0700

    added hello world and accounting apps.

---
+++ Scores/Scores/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Scores")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Scores")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a95daff4-6e49-4884-94a6-6e2a70b52054")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ helloworld/HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!\" \"escaparaoni charecters here!");
+            Console.ReadLine();
+        }
+    }
