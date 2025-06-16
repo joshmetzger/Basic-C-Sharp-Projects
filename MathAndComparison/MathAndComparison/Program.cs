5f2b3a9fe8c151db432f2c6534ea2f9fa9637ec4
joshmetzger <joshmetzger@gmail.com>
2025-06-16 09:24:57 -0700

    added ath and operaters app

---
+++ MathAndComparison/MathAndComparison/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathAndComparison
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int total = 5 + 10;
+
+            //Console.WriteLine("Five plus Ten = " + total);
+            //Console.ReadLine();
+
+            //int total = 10 - 5;
+
+            //Console.WriteLine("Ten minus Five = " + total);
+            //Console.ReadLine();
+
+
+            //get number
+            Console.WriteLine("enter any number: ");
+            //save number
+            string num1 = Console.ReadLine();
+            //convert to num and * 50
+            int number1 = Convert.ToInt32(num1) * 50;
+            Console.WriteLine("the new number is: " + number1);
+            Console.ReadLine();
+
+
+            //get number
+            Console.WriteLine("enter any number: ");
+            //save number
+            string num2 = Console.ReadLine();
+            //convert to num and add 25
+            int number2 = Convert.ToInt32(num2) + 25;
+            Console.WriteLine("the new number is: " + number2);
+            Console.ReadLine();
+
+            //get number
+            Console.WriteLine("enter any number: ");
+            //save number
+            string num3 = Console.ReadLine();
+            //convert to num and divide by 12.5
+            double number3 = Convert.ToDouble(num3) / 12.5;
+            Console.WriteLine("the new number is: " + number3);
+            Console.ReadLine();
+
+            //get number
+            Console.WriteLine("enter any number: ");
+            //save number
+            string num4 = Console.ReadLine();
+            //convert to num, divde by 7, print remainder.
+            double number4 = Convert.ToDouble(num4) % 7;
+            Console.WriteLine("the remainder is: " + number4);
+            Console.ReadLine();
+
+
+        }
+    }
+}
--- VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -15,12 +15,38 @@ namespace VariablesAndDataTypes
             //Console.WriteLine("Your name is: " + youName);
             //Console.ReadLine();
 
-            Console.WriteLine("What is your fav number?");
-            string favNumber = Console.ReadLine();
-            int favNum = Convert.ToInt32(favNumber);
-            int total = favNum + 9;
-            Console.WriteLine("Your fav Number plus 9 is: " + total);
-            Console.ReadLine();
+            //Console.WriteLine("What is your fav number?");
+            //string favNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favNumber);
+            //int total = favNum + 9;
+            //Console.WriteLine("Your fav Number plus 9 is: " + total);
+            //Console.ReadLine();
+
+            int num1 = 34;
+            int num2 = 700;
+            int total1 = num1 + num2;
+            Console.WriteLine(total1);
+
+            int num3 = 34;
+            int num4 = 700;
+            int total2 = num4 - num3;
+            Console.WriteLine(total2);
+
+            int num5 = 3;
+            int num6 = 70;
+            int total3 = num5 * num6;
+            Console.WriteLine(total3);
+
+            int num7 = 34;
+            int num8 = 17;
+            int total4 = num7 / num8;
+            Console.WriteLine(total4);
+
+            string fname = "Yoshy";
+            decimal dec = 84.930134729232m;
+            Console.WriteLine(dec + fname );
+
+
         }
     }
 }
