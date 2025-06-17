34ed580582d52003c10da71033b6e0f5541a3fff
joshmetzger <joshmetzger@gmail.com>
2025-06-17 10:28:44 -0700

    added array ex

---
+++ ArrayEx/ArrayEx/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+
+
+class Program
+{
+    static void Main()
+    {
+        // create an array and instantiate the array object inline.
+        string[] stringArray = new string[] { "blorf", "boff", "beedf", "piff" };
+
+        //convert user's guess into int
+        Console.WriteLine("Enter a number between 0 - " + (stringArray.Length - 1));
+        int number = Convert.ToInt32(Console.ReadLine());
+        // handle if user enters number greater than the length of array
+        if (number > (stringArray.Length - 1))
+        {
+            Console.WriteLine("Please try again, enter a number between 0 - " + (stringArray.Length - 1));
+            number = Convert.ToInt32(Console.ReadLine());
+        }
+        else
+        {
+            //use guess as array index and print out
+            Console.WriteLine(stringArray[number]);
+        }
+
+
+        // create an array and instantiate the array object inline.
+        int[] intArray = new int[] { 300, 15, 86, 75, 100 };
+
+        //convert user's guess into int
+        Console.WriteLine("Enter a number between 0 - " + (intArray.Length - 1));
+        int num = Convert.ToInt32(Console.ReadLine());
+        // handle if user enters number greater than the length of array
+        if (num > (intArray.Length - 1))
+        {
+            Console.WriteLine("Please try again, enter a number between 0 - " + (intArray.Length - 1));
+            num = Convert.ToInt32(Console.ReadLine());
+        }
+        else
+        {
+            //use guess as array index and print out
+            Console.WriteLine(intArray[num]);
+        }
+
+        //CreateInstanceBinder and initialize string list
+        List<string> strList = new List<string>();
+
+        strList.Add("dsfkjn");
+        strList.Add("Yo");
+        strList.Add("Yes");
+        strList.Add("stringing");
+
+        //convert user's guess into int
+        Console.WriteLine("Enter a number between 0 - 3");
+        int index = Convert.ToInt32(Console.ReadLine());
+        // handle if user enters number greater than the length of array
+        if (index > 3)
+        {
+            Console.WriteLine("Please try again, enter a number between 0 - 3");
+            index = Convert.ToInt32(Console.ReadLine());
+        }
+        else
+        {
+            //use guess as array index and print out
+            Console.WriteLine(strList[index]);
+        }
+    }
+}
--- ArraysLists/ArraysLists/Program.cs
+++ ArraysLists/ArraysLists/Program.cs
@@ -6,16 +6,32 @@ class Program
 {
     static void Main()
     {
-        int[] numArray = new int[5];
-        numArray[0] = 5;
-        numArray[1] = 2;
-        numArray[2] = 10;
-        numArray[3] = 1200;
-        numArray[4] = 5000;
+        List<int> intList = new List<int>();
 
-        int[] numArray1 = new int[] { 5, 2, 10, 5000 };
+        intList.Add(4);
+        intList.Add(10);
+        intList.Remove(10);
 
-        Console.WriteLine(numArray1[3]);
-        Console.ReadLine();
+        List<string> stringList = new List<string>();
+
+        stringList.Add("Hi");
+        stringList.Add("Hello");
+
+        Console.WriteLine(stringList[0]);
+
+        //int[] numArray = new int[5];
+        //numArray[0] = 5;
+        //numArray[1] = 2;
+        //numArray[2] = 10;
+        //numArray[3] = 1200;
+        //numArray[4] = 5000;
+
+        //int[] numArray1 = new int[] { 5, 2, 10, 1200, 5000 };
+        //int[] numArray2 = { 5, 2, 10, 200, 5000, 600, 2300 };
+
+        //numArray2[5] = 650;
+
+        //Console.WriteLine(numArray2[5]);
+        //Console.ReadLine();
     }
 }
