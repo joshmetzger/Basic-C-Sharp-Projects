1d3612ca50f4e989d0b8ea598b032de7e78ab1f2
joshmetzger <joshmetzger@gmail.com>
2025-06-19 09:21:40 -0700

    try catch assignment added

---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            Console.WriteLine("Pick a number: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Pick a second number: ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Dividing the two...");
+
+            int num3 = num1 / num2;
+            Console.WriteLine(num1 + " divided by" + num2 + " equals " + num3);
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine("Please type a whole number");
+            return;
+        }
+        catch (DivideByZeroException ex)
+        {
+            Console.WriteLine("Cannot divide by zero");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Console.ReadLine();
+        }
+        
+        
+    }
+}
+
---
+++ StringsIntSubmissionAssignment/StringsIntSubmissionAssignment/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringsIntSubmissionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numList = new List<int>() { 15, 25, 20, 10, 2, 100, 25, 76 };
+
+            while (true)
+            {
+                // exception handling for possible user errors:
+                try
+                {
+                    // get user input
+                    Console.WriteLine("Enter a whole number to divide a list of numbers by: ");
+                    int userNum = Convert.ToInt32(Console.ReadLine());
+
+                    // divide each number in the list by userNum
+                    foreach (int num in numList)
+                    {
+                        Console.WriteLine("result: " + num / userNum);
+                    }
+                }
+                // user enters a number that isn't whole:
+                catch (FormatException ex)
+                {
+                    Console.WriteLine("Please enter only a whole number.");
+                    continue;
+                }
+                // user enters a 0:
+                catch (DivideByZeroException ex)
+                {
+                    Console.WriteLine("Cannot divide by zero, please enter only a whole number.");
+                    continue;
+                }
+                // just in case we left anything out:
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    continue;
+                }
+                // always do this. also Log any info.
+                finally
+                {
+                    Console.ReadLine();
+                }
+                break;
+            }
+            
+        }
+    }
+}
