d826f80515bd40f553342c27b1ed65a8b7ec5847
joshmetzger <joshmetzger@gmail.com>
2025-06-23 10:04:47 -0700

    added method assignment 3

---
+++ MethodAssignments3/MethodAssignments3/MyMath.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignments3
+{
+    public class MyMath
+    {
+        public static int Add(int num1, int num2 = 0)
+        {
+            int result = num1 + num2;
+            return result;
+        }
+    }
+}
---
+++ MethodAssignments3/MethodAssignments3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignments3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //use MyMath class method to add two integers:
+            //int sum = MyMath.Add(2);
+            //Console.WriteLine(sum);
+
+            // prompt user to enter integers. ony the first integer is required.
+            Console.WriteLine("You will be prompted to enter two integers. only the first integer is required, you can skip the second one." +
+                "Enter an integer: ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            //get second optional integer from user
+            Console.WriteLine("Enter a second integer (press enter to skip): ");
+            string input = Console.ReadLine();
+
+            int sum;
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                sum = MyMath.Add(num1);
+            }
+            else
+            {
+                int num2 = Convert.ToInt32(input);
+                sum = MyMath.Add(num1, num2);
+            }
+            Console.WriteLine(sum);            
+
+        }
+    }
+}
