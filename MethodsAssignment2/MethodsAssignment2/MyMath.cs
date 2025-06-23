688f1947d7d0ccf6bfa4c6775495dc0ad256f63a
joshmetzger <joshmetzger@gmail.com>
2025-06-23 09:14:27 -0700

    added method assignment2

---
+++ MethodsAssignment2/MethodsAssignment2/MyMath.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment2
+{
+    public class MyMath
+    {
+        //public static int Double(int num)
+        //{
+        //    int result = num + num;
+        //    return result;
+        //}
+
+        //public static decimal Double(decimal num)
+        //{
+        //    decimal result = num + num;
+        //    return result;
+        //}
+
+        public static int Double(string num)
+        {
+            int number = Convert.ToInt32(num);
+            int result = number + number;
+            return result;
+        }
+    }
+}
---
+++ MethodsAssignment2/MethodsAssignment2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodsAssignment2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //double an integer from class MyMath method
+            //int sum = MyMath.Double(5);
+            //Console.WriteLine(sum);
+
+            //double a decimal from class MyMath method
+            //decimal sum2 = MyMath.Double(10.9472m);
+            //Console.WriteLine(sum2);
+
+            //double a integer that was passed to the method as a string from class MyMath method
+            decimal sum3 = MyMath.Double("10");
+            Console.WriteLine(sum3);
+        }
+    }
+}
