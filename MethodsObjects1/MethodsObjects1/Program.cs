92db088fb87278aeec96bb90007bee50af32039f
joshmetzger <joshmetzger@gmail.com>
2025-06-30 10:05:07 -0700

    updated mothods objects assignment with notes

--- MethodsObjects1/MethodsObjects1/Program.cs
+++ MethodsObjects1/MethodsObjects1/Program.cs
@@ -10,10 +10,12 @@ namespace MethodsObjects1
     {
         static void Main(string[] args)
         {
+            // instantiate object from the inheritted class and set it's properties
             Employee employee1 = new Employee();
             employee1.FirstName = "Sample";
             employee1.LastName = "Student";
 
+            // call the inheritted method from the object's parent class where the method lives
             employee1.SayName();
         }
     }
